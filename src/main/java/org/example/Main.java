fc1099de8218eeb85ce426900742c5c932e63f9a
sakshyam2002 <122495470+sakshyam2002@users.noreply.github.com>
2023-01-30 10:37:21 -0500

    save files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,22 @@ package org.example;
 public class Main {
 
 
-    public static void addStudent() {
+    public static void main(String[] args) {
 
 
-                Student student1 = new Student(200497529, "sakshyam", "pokhrel","running");
+
+            Student student1 = new Student(200497529, "Sakshyam", "pokhrel","running");
             Student student2 = new Student(200578521, "Dipak", "kharal","eating");
-        Student student3 = new Student(200416589, "shyam", "karki","cooking");
-        Student student4 = new Student(200571261, "Dipesh", "khatiwada","playing");
-        Student student5 = new Student(200452029, "sakshi", "pandey","sleeping");
-        Student student6 = new Student(200576951, "sijan", "khatri","working");
-
+            Student student3 = new Student(200416589, "shyam", "karki","cooking");
+            Student student4 = new Student(200571261, "Dipesh", "khatiwada","playing");
+            Student student5 = new Student(200452029, "sakshi", "pandey","sleeping");
 
 
         System.out.println(student1);
-           System.out.println(student2);
+        System.out.println(student2);
+        System.out.println(student3);
+        System.out.println(student4);
+        System.out.println(student5);
         }
 
         }
